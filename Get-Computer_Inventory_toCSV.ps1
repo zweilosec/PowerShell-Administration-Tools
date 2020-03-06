997640d58adb3b9fe1b90ebacbab22462406447a
zweilosec <61819873+zweilosec@users.noreply.github.com>
2020-03-06 16:26:53 +0300

    Create Get-Computer_Inventory_toCSV.ps1
    Given a computer name/list of computer names, produces a csv file with the following information:
    
    Computer_Name	Serial_Number	Current_User	Manufacturer	Model	MAC_Address	Compliance_Status	IP_Address
    
    Todo: Prompt user to add a KB# of patches to check on.  Currently only checks for install status of KB4534276

---
+++ Get-Computer_Inventory_toCSV.ps1
@@ -0,0 +1,75 @@
+Param([string]$Computers)
+
+If (!$Computers) {
+    Write-Output "Please specify a input file with a list of computer names to search for."
+    Write-Output "Format: Get-ComputerInventory.ps1 <filename_with_path>"
+    }
+
+$CSVLogFile = "./ComputerInfo_$(Get-Date -Format yyyymmdd_HHMM).csv"
+$TXTLogFile = "./ComputerInfo_$(Get-Date -Format yyyymmdd_HHMM).txt"
+
+Get-Content $Computers |
+ForEach-Object {
+    $Computer = $_
+    Write-Output "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _"
+    If (Test-Connection -ComputerName $Computer -Count 2 -Quiet) {
+
+        $Serial = Invoke-Command -ComputerName $Computer -ScriptBlock {
+            Get-WmiObject -Class Win32_BIOS | Select -ExpandProperty SerialNumber
+            }
+
+        $CurrentUser = Invoke-Command -ComputerName $Computer -ScriptBlock {
+            Get-WmiObject -Class Win32_ComputerSystem | Select -ExpandProperty UserName
+            }
+        
+        $IPconfigset = Invoke-Command -ComputerName $Computer -ScriptBlock {
+            Get-WmiObject -Class Win32_NetworkAdapterConfiguration
+            }
+
+        $Model = Invoke-Command -ComputerName $Computer -ScriptBlock {
+            Get-WmiObject -Class Win32_ComputerSystem | Select -ExpandProperty Model
+            }
+
+        $Manufacturer = Invoke-Command -ComputerName $Computer -ScriptBlock {
+            Get-WmiObject -Class Win32_ComputerSystem | Select -ExpandProperty Manufacturer
+            }
+
+	    $MACAddress = Invoke-Command -ComputerName $Computer -ScriptBlock {
+	        Get-WmiObject -ClassName Win32_NetworkAdapterConfiguration -Filter "IPEnabled='True'" | 
+	        Select -ExpandProperty MACAddress
+	        }
+        $Compliance = if (Get-HotFix -Id "KB4534276" -ComputerName $Computer -ErrorAction SilentlyContinue) { "Patch KB4534276 Compliant"} else { "Patch KB4534276 Non-compliant"}
+            
+  $IP = '0'
+# Iterate and get IP address
+$count = 0
+foreach ($IPConfig in $IPConfigset) {
+   if ($IPConfig.IPAddress) {
+      foreach ($addr in $IPConfig.IPAddress) {
+   "IP Address   : {0}" -f  $addr;
+    $IP = $addr
+   $count++ 
+   }
+   }
+}
+if ($count -eq 0) {"No IP addresses found"}
+else {"$Count IP addresses found on this system"}
+
+New-Object psobject -Property @{
+                                             Computer_Name = $Computer
+                                             Serial_Number = $Serial
+                                             Current_User  = $CurrentUser
+                                             Manufacturer = $Manufacturer
+                                             Model = $Model
+					     MAC_Address = $MACAddress
+					     Compliance_Status = $Compliance
+					     IP_Address = $IP
+                                            
+           } | Select Computer_Name,Serial_Number,Current_User,Manufacturer,Model,MAC_Address,Compliance_Status,IP_Address |
+               Export-Csv -Append $CSVLogFile -NoTypeInformation
+
+      
+        Write-Output "$Computer | $Serial | $CurrentUser | $Manufacturer | $Model | $MACAddress | $Compliance | $IP"
+    } Else { Write-Output "[x] Failed to ping $Computer." }
+} | Tee -Append $TXTLogFile
+
