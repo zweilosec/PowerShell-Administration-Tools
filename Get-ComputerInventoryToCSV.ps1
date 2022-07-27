ee9aa6b2dd321b8c604a1b00050fc6bf1f08ae5e
WolfZweiler <61819873+zweilosec@users.noreply.github.com>
2022-07-27 09:59:20 -0500

    Wrapped in function for easier use, fixed bug
    Fixed a bug where large WMI caches would cause queries to fail with the error: " Get-CimInstance : The WS-Management service cannot process the request because the XML is invalid." Using the `-Property` argument reduces the amount of data WMI sends to just the specified properties.
    
    Also, wrapped the code in this script inside a function for ease of use in environments where scripts are blocked (or import permanently for future use!

--- Get-ComputerInventoryToCSV.ps1
+++ Get-ComputerInventoryToCSV.ps1
@@ -1,3 +1,5 @@
+Function Get-Inventory
+{
 <#
 .Synopsis
    Script for gathering computer inventory information.
@@ -13,7 +15,6 @@
 .INPUTS
    -ComputerName <computer name>
    Input a list of computer names, either piped in as an object or a text file file with one computer name per line.
-
    -ComplianceKB <KB number>
    Input a single Microsoft KB number related to the specific patch you want to check compliance for.
 .OUTPUTS
@@ -25,7 +26,6 @@
    Computer_Name,Serial_Number,Current_User,Manufacturer,Model,MAC_Address,Compliance_Status,IP_Address
    Example-Comp1,2TK45784PJ,DOMAIN\jane.doe,HP,HP ProBook 650 G2,DE:AD:BE:EF:B5:BF,Patch KB4534276 Compliant,10.10.10.01
    Example-comp2,2TK34684GQ,DOMAIN\john.doe,HP,HP ProBook 650 G2,DE:AD:BE:AF:00:70,Patch KB4534276 Compliant,10.10.10.02
-
    "./ComputerInfo_yyyymmdd_HHMM.txt"
    [ ] - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - [ ]
    Realtek Gaming 2.5GbE Family Controller   : 10.10.10.3
@@ -54,7 +54,7 @@ Param
     [ValidateNotNullOrEmpty()]
     [alias('Name')] #Allows for piping in of computers by name from Active Directory (Get-ADComputer)
     [string[]]
-    $ComputerName, 
+    $ComputerName = $env:COMPUTERNAME, 
 
     #Specific patch to check for compliance. Use Microsoft's KB number.
     #Default is "None Specified"
@@ -86,7 +86,6 @@ Process {
 
     foreach ($Computer in $ComputerName) 
     {
-        Write-Output "[ ] - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - [ ]" | Tee-Object -Append $TXTLogFile
         Write-Verbose "Testing if $Computer is online..."
 
         If (Test-Connection -ComputerName $Computer -Count 2 -Quiet) 
@@ -103,18 +102,18 @@ Process {
             Select-Object -ExpandProperty SerialNumber
 
             #Get Current or last logged in username
-            $CurrentUser = Get-CimInstance -ComputerName $Computer -Class Win32_ComputerSystem | 
+            $CurrentUser = Get-CimInstance -ComputerName $Computer -Class Win32_ComputerSystem -Property UserName  |
             Select-Object -ExpandProperty UserName
 
             #Get list of network adapters
             $IPConfigSet = Get-CimInstance -ComputerName $Computer -Class Win32_NetworkAdapterConfiguration -Filter "IPEnabled='True'"
 
             #Get Model of the PC
-            $Model = Get-CimInstance -ComputerName $Computer -Class Win32_ComputerSystem | 
+            $Model = Get-CimInstance -ComputerName $Computer -Class Win32_ComputerSystem -Property Model |             
             Select-Object -ExpandProperty Model
 
             #Get Manufacturer of the PC
-            $Manufacturer = Get-CimInstance -ComputerName $Computer -Class Win32_ComputerSystem | 
+            $Manufacturer = Get-CimInstance -ComputerName $Computer -Class Win32_ComputerSystem -Property Manufacturer |             
             Select-Object -ExpandProperty Manufacturer
 
             #Get MAC address of the PC
@@ -143,6 +142,7 @@ Process {
 
             Write-Output "`tAdapter Information for $($Computer):" | Tee-Object -Append $TXTLogFile
             Write-Output "`t------------------------------------" | Tee-Object -Append $TXTLogFile
+            
             [String[]]$IP = @()
             # Iterate and get IP addresses for each network interface
             $count = 0
@@ -180,7 +180,7 @@ Process {
                 Current_User  = $CurrentUser
                 Manufacturer = $Manufacturer
                 Model = $Model
-                MAC_Address = $MACAddress[0] #Select only the first interface (Usually built-in Ethernet)
+                MAC_Address = $MACAddress #Select only the first interface (Usually built-in Ethernet)
                 Compliance_Status = $Compliance
                 IP_Address = $IP[0] #Select only the first interface (Usually built-in Ethernet)
             } | 
@@ -189,7 +189,9 @@ Process {
             Export-Csv -Append $CSVLogFile -NoTypeInformation
         
             #Write the output to the console and text log in the following format
-            Write-Output "[+] $Computer | $Serial | $CurrentUser | $Manufacturer | $Model | $($MACAddress[0]) | $Compliance | $($IP[0])" | Tee-Object -Append $TXTLogFile
+            Write-Output "[ ] ComputerName | Serial | CurrentUser | Manufacturer |  Model  | MACAddress | Compliance Checks |    IP   [ ]" | Tee-Object -Append $TXTLogFile
+            Write-Output "[ ] - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - [ ]" | Tee-Object -Append $TXTLogFile
+            Write-Output "[+] $Computer | $Serial | $CurrentUser | $Manufacturer | $Model | $MACAddress | $Compliance | $($IP[0])" | Tee-Object -Append $TXTLogFile
         }
         
         #If unable to ping the computer this will be written to console and text log instead
@@ -207,4 +209,5 @@ End
     Write-Verbose "Scan complete."
     Write-Verbose "$ComputerCount computers were scanned."
     Write-Verbose "$CSVLogFile and $TXTLogFile files created."
+}
 }
